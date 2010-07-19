1e8ef555d737665ae2e3da88201a22c7099491b3
Steven Yvinec-Kruyk <kruken@gmail.com>
2010-07-19 13:15:39 -0700

    added title helper for base title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+	def title
+		base_title = "stevenyk.com"
+		if @title.nil?
+			base_title
+		else
+			"#{@title} // #{base_title}"	
+		end
+	end
 end
