9c34d4bddcffa97ecb63afae14c043136fcc8fae
= <=>
2010-08-11 21:04:36 -0700

    decent user listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,11 @@ class UsersController < ApplicationController
 def index
    @title = "Users"
    @users = User.all
-	
+   		
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @users }
+	
     end
   end
 
