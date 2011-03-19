800ded6d264f31914127d8a199cd1e0032dd539b
= <=>
2011-03-18 21:17:15 -0700

    installed paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110119030509) do
+ActiveRecord::Schema.define(:version => 20110319041539) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(:version => 20110119030509) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "sources", :force => true do |t|
