0bb6a0ed6e0ae5009358e58828af00b38ef58582
Steven Yvinec-Kruyk <kruken@gmail.com>
2011-03-19 21:40:37 -0700

    paperclip installed and functioning for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,11 +14,16 @@
 #
 
 class Post < ActiveRecord::Base
-	attr_accessible(:title, :subject, :body, :body_bg_img, :image, :user_id)
+	attr_accessible(:title, :subject, :body, :body_bg_img, :image, :user_id,:image_file_name, :image_content_type, :image_file_size, :image_updated_at)
 	belongs_to	:user
 	has_many	:tools
 	has_many	:sources
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }	
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :post_image_box => "281X100#"},
+	                  :url  => "/images/assets/post_attachments/:id/:style/:basename.:extension",
+	                  :path => ":rails_root/public/images/assets/post_attachments/:id/:style/:basename.:extension"
+	#validates_attachment_presence :image
+	#validates_attachment_size :image, :less_than => 5.megabytes
+	#validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
 	validates_presence_of(:title, :subject, :body)
 	validates_length_of :title , :maximum => 25
 	validates_length_of :subject , :maximum => 80
--- db/schema.rb
+++ db/schema.rb
@@ -47,13 +47,6 @@ ActiveRecord::Schema.define(:version => 20110319041539) do
     t.string "name"
   end
 
-  create_table "tags_backup", :force => true do |t|
-    t.string   "tag"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "tools", :force => true do |t|
     t.string   "name"
     t.string   "developer"
