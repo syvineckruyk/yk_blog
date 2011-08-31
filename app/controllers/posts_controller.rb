9fc9550225c3ebf362069497d6750b78b69f26c9
Steven Yvinec-Kruyk <steven@stevenyk.com>
2011-08-30 21:30:12 -0700

    added some permission verifications to post views and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,30 +25,35 @@ uses_tiny_mce
   # GET /posts/new
   # GET /posts/new.xml
   def new
-    @post = Post.new
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @post }
+    if signed_in?
+      @post = Post.new
+      respond_to do |format|
+          format.html # new.html.erb
+          format.xml  { render :xml => @post }
+      end
     end
   end
 
   # GET /posts/1/edit
   def edit
-    @post = Post.find(params[:id])
+    if signed_in?
+      @post = Post.find(params[:id])
+    end
   end
 
   # POST /posts
   # POST /posts.xml
   def create
-   @post = Post.create( params[:post] )
-   respond_to do |format|
-      if @post.save
-        format.html { redirect_to(@post, :notice => 'Post was successfully created.') }
-        format.xml  { render :xml => @post, :status => :created, :location => @post }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+    if signed_in?
+      @post = Post.create( params[:post] )
+      respond_to do |format|
+        if @post.save
+          format.html { redirect_to(@post, :notice => 'Post was successfully created.') }
+          format.xml  { render :xml => @post, :status => :created, :location => @post }
+        else
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+        end
       end
     end
   end
@@ -56,15 +61,16 @@ uses_tiny_mce
   # PUT /posts/1
   # PUT /posts/1.xml
   def update
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        format.html { redirect_to(@post, :notice => 'Post was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+    if signed_in?
+      @post = Post.find(params[:id])
+        respond_to do |format|
+        if @post.update_attributes(params[:post])
+          format.html { redirect_to(@post, :notice => 'Post was successfully updated.') }
+          format.xml  { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+        end
       end
     end
   end
@@ -72,12 +78,13 @@ uses_tiny_mce
   # DELETE /posts/1
   # DELETE /posts/1.xml
   def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(posts_url) }
-      format.xml  { head :ok }
+    if signed_in?
+      @post = Post.find(params[:id])
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to(posts_url) }
+        format.xml  { head :ok }
+      end
     end
   end
 end
