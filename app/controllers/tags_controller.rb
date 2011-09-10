8ce071e4b9f3d71fdd77a933dd128ac58dae656d
Steven Yvinec-Kruyk <steven@stevenyk.com>
2011-09-10 14:56:19 -0700

    resolved will_paginate issue in the tags controller by reinstantiating @posts with pagination

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,5 +6,7 @@ def show
   @tag = Tag.find(params[:id])
   @title = "Posts tagged with '#{@tag.name}'"
   @posts = @tag.taggings.collect { |t| t.taggable }
+  @posts = @posts.paginate(:page => 1, :per_page => 2)
+  render(:template => 'posts/index')
 end
 end
