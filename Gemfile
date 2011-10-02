2f34044636b8aafeefc5423e233ceda9a6ea5272
Steven Yvinec-Kruyk <steven@stevenyk.com>
2011-10-01 19:30:55 -0700

    fixes

--- Gemfile
+++ Gemfile
@@ -4,4 +4,3 @@ gem 'tiny_mce'
 gem 'aws-s3'
 gem 'will_paginate','2.3.16'
 gem "rails", "2.3.8"
-
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,7 +7,7 @@ def show
   @tag = Tag.find(params[:id])
   @title = "Posts tagged with '#{@tag.name}'"
   @posts = @tag.taggings.collect { |t| t.taggable }
-  @posts = @posts.paginate( :page => params[:page], :per_page => 3)
+  @posts = @posts.paginate( :page => params[:page], :per_page => 5)
   render(:template => 'posts/index')
 end
 end
